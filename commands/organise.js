c2d8c7e179d852d2e6d1c1958d7e9b13eca61f46
Ayush tambi <61240520+tambi-ayush@users.noreply.github.com>
2021-07-25 19:58:46 +0530

    Update organise.js

--- commands/organise.js
+++ commands/organise.js
@@ -1,6 +1,33 @@
+let fs=require("fs");
+let path=require("path");
+let types={
+    media:["mp4","mkv"],
+    archieves:["zip","7z","rar","tar","gz","ar","iso"],
+    documents:["docx","doc","pdf","xlsx","xls","odt"],
+    app:["exe","dmg","pkg","deb"]
+}
 
-    let v="organise command executed with path ";
+function organiseFn(src){
+     let fileArr=fs.readdirSync(src);
+    for(let item in types){
+        let pathItem=path.join(src,item);
+        fs.mkdirSync(pathItem); //lets first make empty folders
+    }            
+    for(let i=0;i<fileArr.length;i++){//now iterating each file and getting its extension
+                let ext=path.extname(fileArr[i]).substring(1);//now ext will be matched with each object value and put
+                for(let item in types){
+                for(let k=0;k<types[item].length;k++){
+                    if(ext==types[item][k]){
+    
+                        let adPath=path.join(path.join(src,item),fileArr[i]);
+                        fs.copyFileSync(path.join(src,fileArr[i]),adPath);
+                    }
+                }
+            }
+          }
 
+
+}
 module.exports={
-    fxn:v
+    organisefxn:organiseFn
 }
